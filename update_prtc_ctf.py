16983221bfbfbb902191522aaa2c83d002ecdc84
Po-Lin Chiu <plchiu@alanine-plchiulab-io.dhcp.asu.edu>
2017-06-18 11:41:52 -0700

    061817

---
+++ update_prtc_ctf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Update the CTF information in the Relion particle star file. 
+#
+# Po-Lin Chiu    2017.06.18 - Created
+#
+
+def run_ctffind4():
+    "Run CTFFIND4 program."
+    
+
+
+
+in_prtc_star = "your_star_file.star"
+in_image = "your_input_image_for_re_ctffind4.mrc"
+path_ctffind4 = "path_to_your_ctffind4"
+
+
+
